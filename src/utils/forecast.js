9467efda3d72530441c180fa0ab30de823f0014c
Neil Singer <njsinger63@gmail.com>
2020-03-23 10:58:53 -0400

    add to forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) {
       callback('unable to find location', undefined);
     } else {
-      callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+      callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability +
+      '% chance of rain.  Wind is ' + body.currently.windSpeed + ' mph');
     }
   });
 };
